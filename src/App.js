c48066501a197dd804aab86020995a3c8db8bf8b
shubhamtrn <shubhamkumartrn@gmail.com>
2022-09-01 17:11:57 +0530

    Navbar created

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@
 import './App.css';
 import Child from './components/Child';
 import First from './components/First';
+import Navbar from './components/Navbar';
 import Parent from './components/Parent';
 import Second from './components/Second';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <First/> */}
-        {/* <Parent/>
-        <Second/> */}
-
-      </header>
-    </div>   
+    <>
+    <Navbar/>
+    </>
   );
 }
 
